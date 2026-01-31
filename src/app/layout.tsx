770790fbcb426e28f50f00b63a50c7174c8b4c9f
강윤수 <yunsoo@gang-yunsuui-MacBookPro.local>
2026-02-01 01:46:08 +0900

    feat: add Google Analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -29,7 +30,22 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="ko">
-            <body>{children}</body>
+            <body>
+                <Script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=G-VYPNJMY967"
+                />
+                <Script id="google-analytics">
+                    {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-VYPNJMY967');
+            `}
+                </Script>
+                {children}
+            </body>
         </html>
     );
 }
